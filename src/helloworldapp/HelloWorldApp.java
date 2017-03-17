3c515ac9d15d8d6a86fad7c47c9b0bd12eca343b
Eric Abell <crucalculus@gmail.com>
2017-03-17 11:04:54 -0500

    Hello World Tutorial completed!

--- src/helloworldapp/HelloWorldApp.java
+++ src/helloworldapp/HelloWorldApp.java
@@ -6,7 +6,8 @@
 package helloworldapp;
 
 /**
- *
+ * The HelloWorldApp class implements an application that simply prints
+ * "Hello World!" to standard output.
  * @author eabell
  */
 public class HelloWorldApp {
@@ -15,7 +16,7 @@ public class HelloWorldApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World!"); // Display the string.
     }
     
 }
